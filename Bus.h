7e5c52e59253a763c42b3a6b5fd1cd5264470551
riadbettole <118698087+riadbettole@users.noreply.github.com>
2024-06-18 10:42:29 +0100

    Started setting up my 6502 CPU
    I did setup :
    the flags
    the registers
    the adressing modes (imcomplete)
    the operations (imcomplete)
    
    and a 16bits bus connecting the cpu and the ram

---
+++ Bus.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+#include "olc6502.h"
+#include <array>
+
+class Bus
+{
+public:
+	Bus();
+	~Bus();
+
+public: //Devices on bus
+	olc6502 cpu;
+	std::array<uint8_t, 64 * 1024> ram;
+
+public: //Bus read & write
+	void write(uint16_t addr, uint8_t data);
+	uint8_t read(uint16_t addr, bool bReadOnly = false);
+};
+
